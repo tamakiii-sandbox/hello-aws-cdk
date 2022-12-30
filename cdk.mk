b5a3b9a27ea46fc915576dacf5edb7dde8611cfc
tamakiii <tamakiii@users.noreply.github.com>
2022-12-30 22:27:12 +0900

    fix

--- cdk.mk
+++ cdk.mk
@@ -6,28 +6,25 @@ export AWS_REGION := $(shell grep 'AWS_REGION' .env | awk -F= '{print $$2}')
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
-# compile typescript to js
+info:
+	@echo "AWS_PROFILE := $(AWS_PROFILE)"
+	@echo "AWS_REGION := $(AWS_REGION)"
+
 build:
 	npm run build
 
-# watch for changes and compile
 watch:
 	npm run watch
 
-# perform the jest unit tests
 test:
 	npm run test
 
-# deploy this stack to your default AWS account/region
 deploy:
 	cdk deploy
 
-# compare deployed stack with current state
 diff:
 	cdk diff
 
-# emits the synthesized CloudFormation template
 synth:
 	cdk synth
-
 
